f9e2b64224bf89671d95e82f22fe3f771301e190
Maen22 <m3n.ibregeth@gmail.com>
2021-08-06 20:17:38 +0300

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis minus
+      aspernatur beatae illo tenetur eaque ea maiores eius sunt cupiditate
+      molestiae expedita ab iste, quas vel, possimus eos, harum libero.
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non eius
+      laboriosam omnis officiis inventore temporibus necessitatibus,
+      reprehenderit debitis aut quo labore quos ipsa earum voluptatibus numquam
+      aliquam consequatur, iste nihil.
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageHelp",
+});
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,107 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <div class="row q-pa-sm bg-primary">
+      <q-input
+        @keyup.enter="addTask"
+        filled
+        v-model="newTask"
+        placeholder="Add a task"
+        dense
+        bg-color="white"
+        class="col"
+        square
+      >
+        <template v-slot:append>
+          <q-btn round dense flat icon="add" @click="addTask" />
+        </template>
+      </q-input>
+    </div>
+
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-ripple
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        clickable
+        :class="{ 'done bg-blue-1': task.done }"
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            v-model="task.done"
+            class="no-pointer-events"
+            color="primary"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn
+            flat
+            round
+            dense
+            color="primary"
+            icon="delete"
+            @click.stop="deleteTask(index)"
+          />
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <div v-if="!tasks.length" class="no-tasks absolute-center">
+      <q-icon name="check" size="100px" color="primary"></q-icon>
+      <div class="text-h5 text-primary text-center">No Tasks</div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "PageIndex",
+
+  data() {
+    return {
+      newTask: "",
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        title: this.newTask,
+        done: false,
+      });
+      this.newTask = "";
+    },
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "Confirm",
+          message: "Would you like to delete the task?",
+          cancel: true,
+          persistent: true,
+        })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+          this.$q.notify("Task deleted successfully!");
+        });
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+
+.no-tasks {
+  opacity: 0.5;
+}
+</style>
